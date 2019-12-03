476328385aaf6003b8bb17c9988648e348863075
Faisal Aziz <faziz6006@gmail.com>
2019-12-03 17:22:36 -0600

    first sub

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+// FIRST EVENT! BOX around LOGO IN HEADING : 
+
+
+const logo = document.querySelector(".logo-heading");
+
+logo.addEventListener('mouseover', (event) => {
+    
+    logo.style.border ="pink dotted 4px";
+      event.stopPropagation();
+
+  })
